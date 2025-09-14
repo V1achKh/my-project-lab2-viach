4ebe9c8b9b246333b5d3aa5c4899482a86e0be18
V1achKh <VyachKh@yandex.ru>
2025-09-14 19:01:36 +0300

    rpn_calculator_done

---
+++ build/_deps/googletest-build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/viach/my_project_lab_viach/build/_deps/googletest-src
+# Build directory: /home/viach/my_project_lab_viach/build/_deps/googletest-build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googlemock")
